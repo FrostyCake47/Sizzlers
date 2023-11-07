6b5b4b87e23b2e156893edb5256c319c1ecf7fcc
Akash <AwesomeAkash47@gmail.com>
2023-11-07 09:43:27 +0530

    edited

--- script.js
+++ script.js
@@ -4,19 +4,59 @@ const button3 = document.getElementById("button3");
 const button4 = document.getElementById("button4");
 
 const output = document.getElementById("output");
+const quest = document.getElementById("quest");
+var userChoice = -1;
 
-button1.addEventListener("click", function() {
-    document.getElementById("output").innerHTML = "Value: Button 1 Clicked";
-});
 
-button2.addEventListener("click", function() {
-    output.textContent = "Value: Button 2 Clicked";
-});
+buttonListeners(){
+    button1.addEventListener("click", function() {
+        output.textContent = ans;
+        userChoice = 1;
+    
+    });
+    
+    button2.addEventListener("click", function() {
+        output.textContent = "Value: Button 2 Clicked";
+        userChoice = 2;
+    });
+    
+    button3.addEventListener("click", function() {
+        //output.textContent = "Value: Button 3 Clicked";
+        userChoice = 3;
+    });
+    
+    button4.addEventListener("click", function() {
+        //output.textContent = "Value: Button 4 Clicked";
+        userChoice = 4;
+    });
+    
+}
 
-button3.addEventListener("click", function() {
-    output.textContent = "Value: Button 3 Clicked";
-});
 
-button4.addEventListener("click", function() {
-    output.textContent = "Value: Button 4 Clicked";
-});
+function checkAns(userChoice, ans){
+    if (userChoice == -1) {
+        output.textContent = "Click a choice";
+    }
+    else if (userChoice == ans){
+        output.textContent = "Viola right ans";
+        console.log("cmon");
+    }
+    else {
+        output.textContent = "Wrong ans beech";
+    }
+}
+
+function main(){
+    const questData = ["elo whats my name", "idk", "gae", "aman", "aaa", 4];
+    var ans = questData[4];
+    quest.textContent = questData[0];
+    button1.textContent = questData[1];
+    button2.textContent = questData[2];
+    button3.textContent = questData[3];
+    button4.textContent = questData[4];
+
+    buttonListeners();
+    checkAns(userChoice, ans);
+}
+
+main();
